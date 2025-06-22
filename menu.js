0e29928a1e4d382ebc8ee2e18f64a8870c9d3f65
VMaina2003 <victormaina05@gmail.com>
2025-06-22 16:05:01 +0300

    add main menu

--- menu.js
+++ menu.js
@@ -2,3 +2,18 @@ let prompt = require('prompt-sync')({ sigint: true });
 
 // Track user actions
 let actionCount = 0;
+
+function mainMenu() {
+  console.log("\n=== Main Menu ===");
+  console.log("1. Greet Me");
+  console.log("2. Tell a Joke");
+  console.log("3. Show Current Time");
+  console.log("4. Reverse a Word");
+  console.log("5. Check if a Number is Even or Odd");
+  console.log("6. Calculate the Square of a Number");
+  console.log("7. Repeat a Phrase");
+  console.log("8. Convert Celsius to Fahrenheit");
+  console.log("9. Count Down from a Number");
+  console.log("10. Exit");
+  console.log("Hint: Try entering 42 ");
+}
